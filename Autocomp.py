7e2f397d401a3b3890bdc4952b1fa8cc2391675f
William POGRUND <36305066+Humanmademovies@users.noreply.github.com>
2021-02-09 13:04:16 +0100

    Update Autocomp.py

--- Autocomp.py
+++ Autocomp.py
@@ -3,12 +3,8 @@ from tkinter import filedialog
 import os
 root = tk.Tk()
 root.withdraw()
-
 file_path = filedialog.askopenfilename(title="sélectionnez votre script", filetypes=[("fichiers python","*.py;*.pyc")])
-print(file_path)
-
 rep = os.path.dirname(file_path)
 fic = os.path.basename(file_path)
 command = ("cd " +str(rep)+ " & " + "pyinstaller --onefile "+ (fic))
 os.system(command)
-print(command)
